9ac0eec847d1137b3116d5a5fa0f28474288cdc7
dmateen <daniyalmateen22@gmail.com>
2024-07-09 13:08:42 +0500

    Added Secure link

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,4 @@
 export const url =
-  process?.env?.VERCEL_ENV == "production"
-    ? process.env.VERCEL_URL?.concat("/api")
+  process?.env?.VERCEL_ENV === "production"
+    ? `https://${process.env.VERCEL_URL}/api`
     : "http://localhost:3000/api";
