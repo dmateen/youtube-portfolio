7b5ee81aeda677e0969df8203a6c6f248a1fc092
dmateen <daniyalmateen22@gmail.com>
2024-07-09 12:06:13 +0500

    Added Youtube Data

---
+++ src/app/api/fetch-youtube-data/route.js
@@ -0,0 +1,62 @@
+import { NextResponse } from "next/server";
+import { google } from "googleapis";
+import { youtubePortfolioLinks } from "./links";
+
+const API_KEY = process.env.YOUTUBE_DEV;
+
+const youtube = google.youtube({
+  version: "v3",
+  auth: API_KEY,
+});
+
+async function getVideoDetails(videoId) {
+  try {
+    const response = await youtube.videos.list({
+      part: "snippet,statistics",
+      id: videoId,
+    });
+    const video = response.data.items[0];
+    if (video) {
+      const title = video.snippet.title;
+      const channel = video.snippet.channelTitle;
+      const views = video.statistics.viewCount;
+      return { title, channel, views };
+    }
+  } catch (error) {
+    console.error(`Error fetching video details: ${error}`);
+  }
+  return null;
+}
+
+export async function GET() {
+  try {
+    const links = youtubePortfolioLinks;
+    const videoData = [];
+
+    for (const link of links) {
+      let videoId;
+      if (link.includes("youtu.be")) {
+        videoId = link.split("youtu.be/")[1].split("?")[0];
+      } else if (link.includes("youtube.com")) {
+        videoId = link.split("v=")[1].split("&")[0];
+      }
+      const details = await getVideoDetails(videoId);
+      if (details) {
+        videoData.push({
+          title: details.title,
+          channel: details.channel,
+          views: details.views,
+          link: link,
+        });
+      }
+    }
+
+    videoData.sort((a, b) => b.views - a.views);
+    return NextResponse.json(videoData, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error processing links" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/page.js
@@ -0,0 +1,64 @@
+import {
+  TableBody,
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableCell,
+} from "@/components/ui/table";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { url } from "@/utils/urls";
+import { formatNumber } from "@/utils/common";
+
+async function getData() {
+  try {
+    const res = await fetch(`${url}/fetch-youtube-data`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+}
+
+export default async function Page() {
+  const data = await getData();
+
+  return (
+    <main>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead>Channel Name</TableHead>
+            <TableHead>Views</TableHead>
+            <TableHead>Link</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>{item.title}</TableCell>
+              <TableCell>{item.channel}</TableCell>
+              <TableCell>{formatNumber(item.views)}</TableCell>
+              <TableCell>
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                  <FaExternalLinkAlt />
+                </a>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </main>
+  );
+}
+
+export const metadata = {
+  title: "Youtube Portfolio",
+  description: "Youtube Portfolio website",
+};
