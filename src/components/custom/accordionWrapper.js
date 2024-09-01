249eaee19d97e67d883e2edaf7b2245cdf2ffe3f
dmateen <daniyalmateen22@gmail.com>
2024-09-01 21:26:31 +0500

    Build issues fixed

--- src/components/custom/accordionWrapper.js
+++ src/components/custom/accordionWrapper.js
@@ -18,8 +18,8 @@ export function AccordionWrapper() {
         </AccordionTrigger>
         <AccordionContent>
           Certainly! You can view my Upwork profile and reviews from previous
-          clients by clicking here. Feel free to reach out to me there if
-          youprefer to collaborate through Upwork.
+          clients by clicking here. Feel free to reach out to me there if you
+          prefer to collaborate through Upwork.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
@@ -29,23 +29,24 @@ export function AccordionWrapper() {
         <AccordionContent>
           The turnaround time for a script typically depends on the length and
           complexity of the content. For most projects, I can deliver a polished
-          script within 1-3 business days. However, I’m always open to
-          discussing expedited timelines if you’re on a tight schedule
+          script within 1-3 business days. However, I&apos;m always open to
+          discussing expedited timelines if you&apos;re on a tight schedule.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
         <AccordionTrigger>
-          How do you ensure the scripts align with my channel's tone and style?
+          How do you ensure the scripts align with my channel&apos;s tone and
+          style?
         </AccordionTrigger>
         <AccordionContent>
           I start by thoroughly researching your channel and understanding your
-          brand voice. We’ll have a detailed discussion about your preferences,
-          and I’ll ensure that each script matches the tone, style, and audience
-          expectations of your channel.
+          brand voice. We&apos;ll have a detailed discussion about your
+          preferences, and I&apos;ll ensure that each script matches the tone,
+          style, and audience expectations of your channel.
         </AccordionContent>
       </AccordionItem>
 
-      <AccordionItem value="item-3">
+      <AccordionItem value="item-4">
         <AccordionTrigger>What is your pricing structure?</AccordionTrigger>
         <AccordionContent>
           My pricing varies depending on the length and complexity of the
@@ -55,24 +56,24 @@ export function AccordionWrapper() {
         </AccordionContent>
       </AccordionItem>
 
-      <AccordionItem value="item-3">
+      <AccordionItem value="item-5">
         <AccordionTrigger>Do you offer revisions?</AccordionTrigger>
         <AccordionContent>
           Yes, I offer revisions to ensure the final script meets your
-          expectations. My goal is to deliver a script that you’re 100%
-          satisfied with, and I’m happy to make adjustments based on your
+          expectations. My goal is to deliver a script that you&apos;re 100%
+          satisfied with, and I&apos;m happy to make adjustments based on your
           feedback.
         </AccordionContent>
       </AccordionItem>
 
-      <AccordionItem value="item-3">
+      <AccordionItem value="item-6">
         <AccordionTrigger>
           How can I get started with a project?
         </AccordionTrigger>
         <AccordionContent>
           Getting started is simple! Just reach out via the contact form on this
-          site or through my Upwork profile. We’ll discuss your project in
-          detail, and I’ll provide a timeline and quote to get your
+          site or through my Upwork profile. We&apos;ll discuss your project in
+          detail, and I&apos;ll provide a timeline and quote to get your
           scriptwriting journey underway.
         </AccordionContent>
       </AccordionItem>
